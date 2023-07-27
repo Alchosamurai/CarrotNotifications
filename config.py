919e3ecf82395615147e38571ed1ddb2d38fa19a
Alchosamurai <alchosan.py@gmail.com>
2023-07-28 00:33:14 +0300

    del useless info from config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 TOKEN = "6698842289:AAElTkez3QQZts0Jgx0y9dLS2Swg2y0ltSU"
 chanel_id = -903143607
 
-AppName = "alchosanpygmail1690390241"
 AppID = "57562"
-API_key = "57562-f0ea49bb05e8ac6a16ce95604b"
-API_secret = "PRIVATE-57562-vLtuv5ChLU8coUbdTz5wC8NuBn2z9PXV8cUKU6d6PfuvwmbRsd"
-user = "userauthkey-57562-bf15aa2e66ad7f5371d574fc254c0bb5f161a06164d20ea145f003eefc8241"
 API_token = "app.57562.57741d683a51df380a83f2baffac81b0338c29a6cb7d5a50"
 
 sup_names ={
